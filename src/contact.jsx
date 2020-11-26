7b1d5197032ccb09ad794929dd8f2c265d9c12a1
swagatika1101 <47102508+swagatika1101@users.noreply.github.com>
2020-11-26 23:38:05 +0530

    changed css of contact page

--- src/contact.jsx
+++ src/contact.jsx
@@ -8,23 +8,23 @@ const contact = () => {
     <>
       <div>
         <Coverlay />
-        <div>
-          <div class="row">
-            <div class="col-9">
+        <div class="contact_container">
+          <div class="row box">
+            <div class="col-lg-9 col-sm-12">
               <div class="col">
-                <h1>Request Free Consultation</h1>
+                <h3>Request Free Consultation</h3>
               </div>
               <div class="row">
-                <div class="col-6">
+                <div class="col-md-6">
                   <Form />
                 </div>
-                <div class="col-6">
+                <div class="col-md-6">
                   <Map />
                 </div>
               </div>
             </div>
-            <div class="col-3">
-              <h1>Get In Touch</h1>
+            <div class="col-lg-3 col-sm-12">
+              <h3>Get In Touch</h3>
               <h5>Reach Us</h5>
               <List />
             </div>
