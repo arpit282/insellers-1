c6eae32492ab6be2a2030017cd62679f76205268
swagatika1101 <47102508+swagatika1101@users.noreply.github.com>
2020-11-28 21:10:30 +0530

    added style in css file for contact form

--- src/contact_form.jsx
+++ src/contact_form.jsx
@@ -3,43 +3,16 @@ import ReactDOM from "react-dom";
 const contact_form = () => {
   return (
     <>
-      <div>
-        <h1>Contact form</h1>
-        <form>
-          <div className="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <small id="emailHelp" className="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
+      <div class="container_form">
+        <div class="contact-box">
+          <div class="right">
+            <input type="text" class="field" placeholder="Your Name" />
+            <input type="text" class="field" placeholder="Your Email" />
+            <input type="text" class="field" placeholder="Phone" />
+            <textarea placeholder="Message" class="field"></textarea>
+            <button class="form_btn">Send</button>
           </div>
-          <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div className="form-group form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" for="exampleCheck1">
-              Check me out
-            </label>
-          </div>
-          <button type="submit" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        </div>
       </div>
     </>
   );
