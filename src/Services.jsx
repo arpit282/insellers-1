b127e0f26d766125f67e40a4184cffa2da64eddb
swagatika1101 <47102508+swagatika1101@users.noreply.github.com>
2020-12-10 20:49:37 +0530

    commented service brand

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Overlay from "./service_overlay";
 import Card from "./service_card";
-import Brand from "./service_brand";
+// import Brand from "./service_brand";
 
 const services = () => {
   return (
     <>
       <Overlay />
       <Card />
-      <Brand />
+      {/* <Brand /> */}
     </>
   );
 };
