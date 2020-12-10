b864e2240d12aef28e2dbcfe2ae686f8b38aa4f9
swagatika1101 <47102508+swagatika1101@users.noreply.github.com>
2020-12-10 13:33:12 +0530

    added tab content

--- src/SignIn_SignUp_form.jsx
+++ src/SignIn_SignUp_form.jsx
@@ -1,18 +1,83 @@
-import React from "react";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import SignIn from "./SignIn";
+// import SignUp from "./SignUp";
+// import ChangeTab from "./popup";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-import ChangeTab from "./popup";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Card,
+  Button,
+  CardTitle,
+  CardText,
+  Row,
+  Col
+} from "reactstrap";
 
-const SignIn_SignUp_form = () => {
-  return (
-    <>
+// const SignIn_SignUp_form = () => {
+//   return (
+//     <>
+//       <div class="form">
+//         <div class="head" className="signup">
+//           <div onclick="changeTab(this)" data-tab="login" class="login-tab">
+//             Login
+//           </div>
+//           <div onclick="changeTab(this)" data-tab="signup" class="signup-tab">
+//             Signup
+//           </div>
+//         </div>
+//         <div class="body" id="form-body">
+//           <SignIn />
+//           <SignUp />
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default SignIn_SignUp_form;
+class SignIn_SignUp_form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeLogin: false,
+      activeSignUp: false
+    };
+  }
+
+  render() {
+    return (
       <div class="form">
         <div class="head" className="signup">
-          <div onclick="changeTab(this)" data-tab="login" class="login-tab">
+          <div
+            onClick={() =>
+              this.setState({
+                activeLogin: true,
+                activeSignUp: false
+              })
+            }
+            data-tab="login"
+            className={this.state.activeLogin ? "active_class" : null}
+          >
             Login
           </div>
-          <div onclick="changeTab(this)" data-tab="signup" class="signup-tab">
+          <div
+            onClick={() =>
+              this.setState({
+                activeLogin: false,
+                activeSignUp: true
+              })
+            }
+            data-tab="signup"
+            className={this.state.activeSignUp ? "active_class" : null}
+          >
             Signup
           </div>
         </div>
@@ -21,8 +86,7 @@ const SignIn_SignUp_form = () => {
           <SignUp />
         </div>
       </div>
-    </>
-  );
-};
-
+    );
+  }
+}
 export default SignIn_SignUp_form;
