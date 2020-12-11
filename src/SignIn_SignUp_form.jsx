5d13226aa8ce757f395b0d043d0fe7d2fb3c9dee
swagatika1101 <47102508+swagatika1101@users.noreply.github.com>
2020-12-11 22:30:25 +0530

    made changes in css file

--- src/SignIn_SignUp_form.jsx
+++ src/SignIn_SignUp_form.jsx
@@ -1,83 +1,35 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import SignIn from "./SignIn";
-// import SignUp from "./SignUp";
-// import ChangeTab from "./popup";
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-import {
-  TabContent,
-  TabPane,
-  Nav,
-  NavItem,
-  NavLink,
-  Card,
-  Button,
-  CardTitle,
-  CardText,
-  Row,
-  Col
-} from "reactstrap";
+import ChangeTab from "./popup";
+// import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+// import SignIn from "./SignIn";
+// import SignUp from "./SignUp";
+// import {
+//   TabContent,
+//   TabPane,
+//   Nav,
+//   NavItem,
+//   NavLink,
+//   Card,
+//   Button,
+//   CardTitle,
+//   CardText,
+//   Row,
+//   Col
+// } from "reactstrap";
 
-// const SignIn_SignUp_form = () => {
-//   return (
-//     <>
-//       <div class="form">
-//         <div class="head" className="signup">
-//           <div onclick="changeTab(this)" data-tab="login" class="login-tab">
-//             Login
-//           </div>
-//           <div onclick="changeTab(this)" data-tab="signup" class="signup-tab">
-//             Signup
-//           </div>
-//         </div>
-//         <div class="body" id="form-body">
-//           <SignIn />
-//           <SignUp />
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default SignIn_SignUp_form;
-class SignIn_SignUp_form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeLogin: false,
-      activeSignUp: false
-    };
-  }
-
-  render() {
-    return (
+const SignIn_SignUp_form = () => {
+  return (
+    <>
       <div class="form">
         <div class="head" className="signup">
-          <div
-            onClick={() =>
-              this.setState({
-                activeLogin: true,
-                activeSignUp: false
-              })
-            }
-            data-tab="login"
-            className={this.state.activeLogin ? "active_class" : null}
-          >
+          <div onclick="changeTab(this)" data-tab="login" class="login-tab">
             Login
           </div>
-          <div
-            onClick={() =>
-              this.setState({
-                activeLogin: false,
-                activeSignUp: true
-              })
-            }
-            data-tab="signup"
-            className={this.state.activeSignUp ? "active_class" : null}
-          >
+          <div onclick="changeTab(this)" data-tab="signup" class="signup-tab">
             Signup
           </div>
         </div>
@@ -86,7 +38,55 @@ class SignIn_SignUp_form extends Component {
           <SignUp />
         </div>
       </div>
-    );
-  }
-}
+    </>
+  );
+};
+
 export default SignIn_SignUp_form;
+// class SignIn_SignUp_form extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       activeLogin: false,
+//       activeSignUp: false
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div class="form">
+//         <div class="head" className="signup">
+//           <div
+//             onClick={() =>
+//               this.setState({
+//                 activeLogin: true,
+//                 activeSignUp: false
+//               })
+//             }
+//             data-tab="login"
+//             className={this.state.activeLogin ? "active_class" : null}
+//           >
+//             Login
+//           </div>
+//           <div
+//             onClick={() =>
+//               this.setState({
+//                 activeLogin: false,
+//                 activeSignUp: true
+//               })
+//             }
+//             data-tab="signup"
+//             className={this.state.activeSignUp ? "active_class" : null}
+//           >
+//             Signup
+//           </div>
+//         </div>
+//         <div class="body" id="form-body">
+//           <SignIn />
+//           <SignUp />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// export default SignIn_SignUp_form;
